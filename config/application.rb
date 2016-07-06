d47085c0b0b98cf133df8d7d6e9d30fb0a903326
Joaquim <pgrilo92@hotmail.com>
2016-07-06 17:33:05 +0000

    Updated S3

--- config/application.rb
+++ config/application.rb
@@ -21,14 +21,6 @@ module The6lord
       end
     end
 
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-      :bucket => ENV['S3_BUCKET'],
-      :access_key_id => ENV['S3_ACCESS_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
-    }
-}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,15 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
