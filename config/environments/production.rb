a98e61092f387104156e11f18426c22d63851a42
Joaquim Grilo <pgrilo92@hotmail.com>
2016-07-15 19:37:22 +0000

    add SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'the-6lord-joaquim.herokuapp.com', :protocol => 'https' }
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
