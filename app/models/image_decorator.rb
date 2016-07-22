6f9e33067c1e16001b1dedd9b83bbb3d97f58066
Joaquim Grilo <pgrilo92@hotmail.com>
2016-07-22 17:15:36 +0000

    new size

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -2,7 +2,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
     :mini => '48x48>', # thumbs under image
     :small => '100x100>', # images on category view
-    :medium => '160x160', 
+    :medium => '110x130', 
     :product => '240x240>', # full product image
     :large => '600x600>' # light box image
   }
