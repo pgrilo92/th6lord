93dcdc854f4c78c1ff0a7788afc808484097c4e0
Joaquim Grilo <pgrilo92@hotmail.com>
2016-07-31 21:27:53 +0000

    new update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,6 @@ config.assets.precompile = ['.js', '.css', '*.css.erb']
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'the-6lord-joaquim.herokuapp.com', :protocol => 'https' }
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
