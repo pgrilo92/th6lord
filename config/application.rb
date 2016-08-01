c2a99bb24440d009d076febc717149700912cf4d
Joaquim Grilo <pgrilo92@hotmail.com>
2016-08-01 17:09:48 +0000

    image

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,6 @@ module The6lord
     config.active_record.raise_in_transactional_callbacks = true
  
     config.assets.initialize_on_precompile = false
+    config.serve_static_assets = true
   end
 end
